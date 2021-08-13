ec943a7777b2388f3e73b589a939b7419052e372
Kimyoondeok <centurio87@naver.com>
2021-08-13 18:02:40 +0900

    regular commit

---
+++ python/1002.py
@@ -0,0 +1,17 @@
+import unittest
+import itertools
+
+def impl1002():
+  N = int(input())
+  caseArr = []
+  for _ in range(N):
+    case = map(int, input().split())
+    itertools.
+    caseArr.insert()
+    
+class Test1002(unittest.TestCase):
+  def test1002(self):
+    self.assertEqual(impl1002(), [2,1,0])
+
+if __name__ == '__main__':
+  unittest.main()
